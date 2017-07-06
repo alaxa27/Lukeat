2cf1348c238faf57518996758ee02acfab0f5274
OsamaMM <extra.time.om@gmail.com>
2017-07-06 20:36:11 +0200

    home + animations

--- js/components/home/index.js
+++ js/components/home/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { Image, View, StatusBar } from "react-native";
-
+import * as Animatable from 'react-native-animatable'
 import { Container, Button, H3, Text, Header, Title, Body, Left, Right } from "native-base";
 
 import styles from "./styles";
@@ -17,21 +17,25 @@ class Home extends Component {
 				<StatusBar barStyle="light-content" />
 				<Image source={launchscreenBg} style={styles.imageContainer}>
 					<View style={styles.logoContainer}>
-						<Image source={launchscreenLogo} style={styles.logo} />
+						<Animatable.Image animation='fadeIn' duration={700} source={launchscreenLogo} style={styles.logo} />
 					</View>
-					<View style={{ flex: 1, alignItems: 'stretch', marginBottom: 130, marginHorizontal: 100 }}>
-						<Button
-							style={{ backgroundColor: "white", alignSelf: "center", marginBottom: 40, width: 150 }}
-							onPress={() => this.props.navigation.navigate("SignupForm")}
-						>
-							<Text style={{textAlign: 'center', color: "#6FAF98", marginLeft: 27, fontWeight: 'bold', fontSize: 16 }}>Sign up</Text>
-						</Button>
-						<Button
-							style={{ backgroundColor: "white", alignSelf: "center", width: 150 }}
-							onPress={() => this.props.navigation.navigate("LoginForm")}
-						>
-							<Text style={{textAlign: 'center', color: "#6FAF98", marginLeft: 33, fontWeight: 'bold', fontSize: 16 }}>Log in</Text>
-						</Button>
+					<View style={{ flex: 1, marginBottom: 130 }}>
+						<Animatable.View animation='bounceIn' iterationCount={2} delay={350}>
+							<Button
+								style={{ backgroundColor: "white", alignSelf: "center", marginBottom: 40, width: 150, borderRadius: 5 }}
+								onPress={() => this.props.navigation.navigate("SignupForm")}
+							>
+								<Text style={{textAlign: 'center', color: "#6FAF98", marginLeft: 27, fontWeight: 'bold', fontSize: 16 }}>Sign up</Text>
+							</Button>
+						</Animatable.View>
+						<Animatable.View animation='bounceInUp' delay={250}>
+							<Button
+								style={{ backgroundColor: "white", alignSelf: "center", width: 150, borderRadius: 5 }}
+								onPress={() => this.props.navigation.navigate("LoginForm")}
+							>
+								<Text style={{textAlign: 'center', color: "#6FAF98", marginLeft: 33, fontWeight: 'bold', fontSize: 16 }}>Log in</Text>
+							</Button>
+						</Animatable.View>
 					</View>
 				</Image>
 			</Container>
